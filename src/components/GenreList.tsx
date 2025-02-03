896598c8ee0d231d9b8716121418350994863173
josh <josh.kilov@gmail.com>
2025-02-03 16:56:03 +1100

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,22 +2,35 @@ import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
+
 const GenreList = () => {
-  const { data, isLoading, error } = useGenres();
+ const { data, isLoading, error } = useGenres();
 
-  if (error) return null;
-  if (isLoading) return < Spinner />
 
-  return (
-    <List>
-      {data.map((genre) => (
+ if (isLoading) return < Spinner />
+ if (error) return null;
 
-        <ListItem paddingY='5px' key={genre.id}><HStack> < Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-        <Text fontSize='lg'>{genre.name}</Text>
-        </HStack></ListItem>
-      ))}
-    </List>
-  );
+
+
+
+
+
+
+
+ return (
+
+
+
+
+   <List>
+     {data.map((genre) => (
+       <ListItem paddingY='5px' key={genre.id}><HStack> < Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+       <Text fontSize='lg'>{genre.name}</Text>
+       </HStack></ListItem>
+     ))}
+   </List>
+ );
 };
+
 
 export default GenreList;
