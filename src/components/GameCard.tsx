dfb1f0696ef9e6ec462b29121e93c91f7a1d65da
josh <josh.kilov@gmail.com>
2025-02-16 10:10:26 +1100

    Clean Up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -19,11 +19,12 @@ return (
   <Card>
       < Image src={getCroppedImageUrl(game.background_image)}/>
       <CardBody>
-          < Heading fontSize='2xl'>{game.name}</Heading>
-          <HStack justifyContent='space-between'>
+      <HStack justifyContent='space-between' marginBottom={3}>
           < PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
           < CriticScore score={game.metacritic}/>
-          </HStack>
+          </HStack> 
+          < Heading fontSize='2xl'>{game.name}</Heading>
+         
       </CardBody>
   </Card>
 )
